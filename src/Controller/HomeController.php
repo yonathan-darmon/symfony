60b53cf5e8496e9562bc1fa5d652b6a2e94fa8c8
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-09-21 12:13:29 +0200

    redirect home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        if ($this->getUser()) {
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        } else {
+            return $this->redirectToRoute('login');
+        }
     }
 }
