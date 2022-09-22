5c637f3630c5c9d9a31caed1c9e2ea314fb58934
yonathan-darmon <yonathan.darmon@laplateforme.io>
2022-09-22 17:07:41 +0200

    profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    public function index(Request $request, UserInterface $user): Response
     {
+        $userid=$this->getUser()->getId();
+        
         return $this->render('profil/index.html.twig', [
             'controller_name' => 'ProfilController',
         ]);
